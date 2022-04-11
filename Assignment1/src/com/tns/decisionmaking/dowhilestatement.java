467e276adc78b086893e7c6895f4e2603827c86f
Anoop Kumar Prasad <31107540+AnoopKumarPrasad@users.noreply.github.com>
2022-04-11 13:38:16 +0530

    First commit

--- Assignment1/src/com/tns/decisionmaking/dowhilestatement.java
+++ Assignment1/src/com/tns/decisionmaking/dowhilestatement.java
@@ -3,8 +3,12 @@ package com.tns.decisionmaking;
 public class dowhilestatement {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int x=1;
+		do 
+		{
+		System.out.println("Fire!!" + x);
+		x++;
+		} 
+		while (x<10);
 	}
-
 }
