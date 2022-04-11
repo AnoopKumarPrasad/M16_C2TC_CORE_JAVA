8146678c66facfcce8668752baa52a7d80f363de
Anoop Kumar Prasad <31107540+AnoopKumarPrasad@users.noreply.github.com>
2022-04-11 13:44:02 +0530

    First commit

---
+++ Assignment1/src/com/tns/looping/enhancedforloop.java
@@ -0,0 +1,12 @@
+package com.tns.looping;
+
+public class enhancedforloop {
+
+	public static void main(String[] args) {
+		String array[] = {"Anoop", "Amey", "Amlan"};
+		for (String x:array)
+		{
+			System.out.println(x);
+		}
+	}
+}
