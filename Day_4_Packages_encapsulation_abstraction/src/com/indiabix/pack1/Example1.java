774cda1e57fe95319da9e0994e48d72941839ec2
Anoop Kumar Prasad <31107540+AnoopKumarPrasad@users.noreply.github.com>
2022-04-13 12:51:11 +0530

    First Commit

---
+++ Day_4_Packages_encapsulation_abstraction/src/com/indiabix/pack1/Example1.java
@@ -0,0 +1,9 @@
+package com.indiabix.pack1;
+
+public class Example1 {
+
+	public void display()
+		{
+			System.out.println("Welcome to M16");
+		}
+	}
---
+++ Day_4_Packages_encapsulation_abstraction/src/com/tns/pack2/Example2.java
@@ -0,0 +1,13 @@
+package com.tns.pack2;
+
+import com.indiabix.pack1.*;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Example1 e=new Example1();
+		e.display();
+	}
+
+}
