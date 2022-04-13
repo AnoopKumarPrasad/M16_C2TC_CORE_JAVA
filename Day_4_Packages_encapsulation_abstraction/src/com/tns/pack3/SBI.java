a15a45739ec5a198400f840d7d51d33d4135f660
Anoop Kumar Prasad <31107540+AnoopKumarPrasad@users.noreply.github.com>
2022-04-13 13:19:13 +0530

    First commit

---
+++ Day_4_Packages_encapsulation_abstraction/src/com/tns/pack3/SBI.java
@@ -0,0 +1,16 @@
+package com.tns.pack3;
+
+public class SBI
+{
+
+	private int balance;
+	public int getBalance()
+	{
+		return balance;
+	}
+	public void setBalance(int bal)
+	{
+		balance=bal;
+	}
+
+}
---
+++ Day_4_Packages_encapsulation_abstraction/src/com/tns/pack4/Encapsulation.java
@@ -0,0 +1,14 @@
+package com.tns.pack4;
+import com.tns.pack3.SBI;
+
+public class Encapsulation {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		SBI obj=new SBI();
+		obj.setBalance(57000);
+		System.out.println(obj.getBalance());
+	}
+
+}
