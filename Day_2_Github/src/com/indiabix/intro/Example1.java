dc582c2925c4bdb29c61ed85734ba1ce31ce7938
Anoop Kumar Prasad <31107540+AnoopKumarPrasad@users.noreply.github.com>
2022-04-17 15:01:03 +0530

    First commit

---
+++ Day_2_Github/src/com/indiabix/intro/Example1.java
@@ -0,0 +1,19 @@
+package com.indiabix.intro;
+
+public class Example1 {
+
+	public static void main(String[] args) 
+	{
+		int a=15;//whole
+		float b=10.5f;//decimal point
+		double c=14.7;//decimal point
+		char d='x';
+		String str="Anoop Kumar Prasad";
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(str);
+	}
+
+}
---
+++ Day_2_Github/src/com/indiabix/intro/Example2.java
@@ -0,0 +1,17 @@
+package com.indiabix.intro;
+
+import java.util.Scanner;
+
+public class Example2 {
+
+	public static void main(String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		System.out.println(a);
+		s.close();
+		
+
+	}
+
+}
