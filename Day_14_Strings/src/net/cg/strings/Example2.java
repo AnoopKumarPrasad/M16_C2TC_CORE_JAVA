e88e1339a0c283a4331746776e6028218e53b07b
Anoop Kumar Prasad <31107540+AnoopKumarPrasad@users.noreply.github.com>
2022-04-26 16:32:07 +0530

    First commit

---
+++ Day_14_Strings/src/net/cg/strings/Example2.java
@@ -0,0 +1,16 @@
+package net.cg.strings;
+
+import java.util.Scanner;
+public class Example2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Scanner s=new Scanner(System.in);
+		String str1="Hi";
+		String str2="Hi";
+		System.out.println(str1=str2);
+		//Scanner is a object for class S
+		//(strl.equals(strl2)
+			}
+
+}
---
+++ Day_14_Strings/src/net/cg/strings/Example3.java
@@ -0,0 +1,14 @@
+package net.cg.strings;
+
+public class Example3 {
+
+	public static void main(String[] args) {
+		String s= 50 + 50 + "hey" + 80+ "anoop" +50 + 50 + 20 + "Hey"; //after encountering string the java starts concatentating it
+		String y=50+ 50 + 20 + "Shi"; //to again start + as summation use different line
+		System.out.println(s);
+		System.out.println(y);
+
+
+	}
+
+}
