853a68f3bbf4d98e5fdd71735067dab05fd72496
Anoop Kumar Prasad <31107540+AnoopKumarPrasad@users.noreply.github.com>
2022-04-26 15:50:24 +0530

    First commit

---
+++ Day_14_Strings/src/net/cg/strings/Example1.java
@@ -0,0 +1,15 @@
+package net.cg.strings;
+
+import java.util.Scanner;
+public class Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		String strl=s.nextLine();
+		System.out.println(strl);
+		
+		//(strl.equals(strl2)
+	}
+
+}
